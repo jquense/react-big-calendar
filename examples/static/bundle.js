521b8e234850af93dfcc15bc23d484f5e1ca0a24
jquense <monastic.panic@gmail.com>
2015-10-28 15:05:26 +0700

    rebuild

--- examples/static/bundle.js
+++ examples/static/bundle.js
@@ -41168,71 +41168,50 @@
 /* 391 */
 /***/ function(module, exports) {
 
-	// import Chance from 'chance';
-
-	// let chance = new Chance();
-	// let events = []
-
-	// for (var i = 0; i < 300; i++) {
-	//   events.push({
-	//     start: chance.date({ year: 2015 }),
-	//   })
-
 	"use strict";
 
 	exports.__esModule = true;
-	exports["default"] = [
-	// {
-	//   "title": "All Day Event",
-	//   "allDay": true,
-	//   "start": new Date(2015, 3, 0),
-	//   "end": new Date(2015, 3, 0)
-	// },
-	// {
-	//   "title": "Long Event",
-	//   "start": new Date(2015, 3, 7),
-	//   "end": new Date(2015, 3, 10),
-	// },
-	// {
-	//   "title": "Some Event",
-	//   "start": new Date(2015, 3, 9, 0, 0, 0),
-	//   "end": new Date(2015, 3, 9, 0, 0, 0),
-	// },
-	{
+	exports["default"] = [{
+	  "title": "All Day Event",
+	  "allDay": true,
+	  "start": new Date(2015, 3, 0),
+	  "end": new Date(2015, 3, 0)
+	}, {
+	  "title": "Long Event",
+	  "start": new Date(2015, 3, 7),
+	  "end": new Date(2015, 3, 10)
+	}, {
+	  "title": "Some Event",
+	  "start": new Date(2015, 3, 9, 0, 0, 0),
+	  "end": new Date(2015, 3, 9, 0, 0, 0)
+	}, {
 	  "title": "Conference",
 	  "start": new Date(2015, 3, 11),
 	  "end": new Date(2015, 3, 13)
 	}, {
 	  "title": "Meeting",
 	  "start": new Date(2015, 3, 12, 10, 30, 0, 0),
-	  "end": new Date(2015, 3, 13, 12, 30, 0, 0)
+	  "end": new Date(2015, 3, 12, 12, 30, 0, 0)
 	}, {
 	  "title": "Lunch",
 	  "start": new Date(2015, 3, 12, 12, 0, 0, 0),
-	  "end": new Date(2015, 3, 13, 13, 0, 0, 0)
-	},
-	// {
-	//   "title": "Meeting",
-	//   "start":new Date(2015, 3, 12,14, 0, 0, 0),
-	//   "end": new Date(2015, 3, 13,15, 0, 0, 0)
-	// },
-	// {
-	//   "title": "Happy Hour",
-	//   "start":new Date(2015, 3, 12, 17, 0, 0, 0),
-	//   "end": new Date(2015, 3, 13, 17, 30, 0, 0)
-	// },
-	{
+	  "end": new Date(2015, 3, 12, 13, 0, 0, 0)
+	}, {
+	  "title": "Meeting",
+	  "start": new Date(2015, 3, 12, 14, 0, 0, 0),
+	  "end": new Date(2015, 3, 12, 15, 0, 0, 0)
+	}, {
+	  "title": "Happy Hour",
+	  "start": new Date(2015, 3, 12, 17, 0, 0, 0),
+	  "end": new Date(2015, 3, 12, 17, 30, 0, 0)
+	}, {
 	  "title": "Dinner",
 	  "start": new Date(2015, 3, 12, 20, 0, 0, 0),
-	  "end": new Date(2015, 3, 13, 21, 0, 0, 0)
+	  "end": new Date(2015, 3, 12, 21, 0, 0, 0)
 	}, {
 	  "title": "Birthday Party",
 	  "start": new Date(2015, 3, 13, 7, 0, 0),
 	  "end": new Date(2015, 3, 13, 10, 30, 0)
-	}, {
-	  "title": "Birthday Party 2",
-	  "start": new Date(2015, 3, 13, 7, 0, 0),
-	  "end": new Date(2015, 3, 13, 7, 0, 0)
 	}];
 	module.exports = exports["default"];
 

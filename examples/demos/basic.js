23d6bda45bc7db8a5d464ee735cedf84c2a533f1
Jason Quense <monastic.panic@gmail.com>
2016-01-14 14:32:43 -0500

    visual test

--- examples/demos/basic.js
+++ examples/demos/basic.js
@@ -5,12 +5,10 @@ import events from '../events';
 let Basic = React.createClass({
   render(){
     return (
-      <div>
-        <BigCalendar
-          events={events}
-          defaultDate={new Date(2015, 3, 1)}
-        />
-      </div>
+      <BigCalendar
+        events={events}
+        defaultDate={new Date(2015, 3, 1)}
+      />
     )
   }
 })

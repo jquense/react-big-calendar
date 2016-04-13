46319970066327c286799de8eb047508d1afa008
Harel Malka <harel@harelmalka.com>
2016-04-13 02:27:05 +0100

    Fix first day of the week evaluation when using Globalize
    * fix the Globalize lcalizer to properly return first day of the week from the CLDR. In case CLDR supplemental is not loaded, or any other failure, the original Math based method to get first day of week will be used as a fallback.
    
    * console.error when cannot use supllemental
    
    * fix day typo
    
    * fix packages files: from lib/ to lib/*
    
    * fix packages files: removed files
    
    * removed debug and updated warning message when supplemental fails
    
    * revert to fr being default locale on demo
    
    * change console.warn to invariant
    
    * changed to warning from invariant

--- src/localizers/globalize.js
+++ src/localizers/globalize.js
@@ -1,5 +1,6 @@
 import dates from '../utils/dates';
 import oldGlobalize from './oldGlobalize';
+import warning from 'warning';
 import { set } from '../formats';
 import { set as setLocalizer } from '../localizer';
 
@@ -37,13 +38,30 @@ export let formats = {
 export default function(globalize) {
   let locale = culture => culture ? globalize(culture) : globalize;
 
+  // return the first day of the week from the locale data. Defaults to 'world'
+  // territory if no territory is derivable from CLDR.
+  // Failing to use CLDR supplemental (not loaded?), revert to the original
+  // method of getting first day of week.
   function firstOfWeek(culture) {
-    let date = new Date();
-    //cldr-data doesn't seem to be zero based
-    let localeDay = Math.max(
-      parseInt(locale(culture).formatDate(date, { raw: 'e' }), 10) - 1, 0)
+    try {
+        const days = ['sun', 'mon', 'tue', 'wed', 'thu', 'fri', 'sat'];
+        const cldr = locale(culture).cldr;
+        const territory = cldr.attributes.territory;
+        const weekData = cldr.get('supplemental').weekData;
+        const firstDay = weekData.firstDay[territory || '001'];
+        return days.indexOf(firstDay);
+    } catch (e) {
+        warning(true,
+            `Failed to accurately determine first day of the week.
+            Is supplemental data loaded into CLDR?`);
+        // maybe cldr supplemental is not loaded? revert to original method
+        const date = new Date();
+        //cldr-data doesn't seem to be zero based
+        let localeDay = Math.max(
+          parseInt(locale(culture).formatDate(date, { raw: 'e' }), 10) - 1, 0)
 
-    return Math.abs(date.getDay() - localeDay)
+        return Math.abs(date.getDay() - localeDay)
+    }
   }
 
   if (!globalize.load)

280b5c77d089a8009a401c5a477923cdfe386cf6
Gregory Beaver <greg@chiaraquartet.net>
2016-05-20 23:20:41 -0500

    fix the story to show off the selection with slices

--- src/components/.stories/DaySlot.jsx
+++ src/components/.stories/DaySlot.jsx
@@ -10,52 +10,57 @@ import { debug } from '../../containers/Selection.jsx'
 storiesOf('components.DaySlot', module)
   .add('default view', () => {
     return (
-      <div style={{height: '80%'}}>
-        <DaySlot style={{width: 100}} min={moment('9:00am', 'h:mma').toDate()}
-                    max={moment('5:00pm', 'h:mma').toDate()}
-                    now={moment('9:20am', 'h:mma').toDate()}
-                 formatter={({start, end}) => {
+      <DaySlot style={{width: 100}} min={moment('9:00am', 'h:mma').toDate()}
+                  max={moment('5:00pm', 'h:mma').toDate()}
+                  now={moment('9:20am', 'h:mma').toDate()}
+               formatter={({start, end}) => {
+                return `${moment(start).format('h:mm')}-${
+                moment(end).format('h:mm')}`
+               }}
+      />
+    )
+  })
+
+  .add('3 slices', () => {
+    return (
+      <DaySlot style={{width: 100}} min={moment('9:00am', 'h:mma').toDate()}
+               max={moment('5:00pm', 'h:mma').toDate()}
+               now={moment('9:23am', 'h:mma').toDate()}
+               slices={3}
+               formatter={({start, end}) => {
                   return `${moment(start).format('h:mm')}-${
                   moment(end).format('h:mm')}`
                  }}
-        />
-      </div>
-    )
-  })
-
-  .add('time is 9:23am', () => {
-    return (
-      <DaySlot style={{width: 100}} min={moment('9:00am', 'h:mma').toDate()}
-                  max={moment('5:00pm', 'h:mma').toDate()}
-                  now={moment('9:23am', 'h:mma').toDate()}
       />
     )
   })
 
-  .add('time is 9:19:59am', () => {
+
+  .add('4 slices', () => {
     return (
       <DaySlot style={{width: 100}} min={moment('9:00am', 'h:mma').toDate()}
-                  max={moment('5:00pm', 'h:mma').toDate()}
-                  now={moment('9:19:59am', 'h:mm:ssa').toDate()}
+               max={moment('5:00pm', 'h:mma').toDate()}
+               now={moment('9:23am', 'h:mma').toDate()}
+               slices={4}
+               formatter={({start, end}) => {
+                  return `${moment(start).format('h:mm')}-${
+                  moment(end).format('h:mm')}`
+                 }}
       />
     )
   })
 
-  .add('time is 9:20:01am', () => {
-    return (
-      <DaySlot style={{width: 100}} min={moment('9:00am', 'h:mma').toDate()}
-                  max={moment('5:00pm', 'h:mma').toDate()}
-                  now={moment('9:20:01am', 'h:mm:ssa').toDate()}
-      />
-    )
-  })
 
-  .add('time is 9:20:01am, 5 slices', () => {
+  .add('5 slices', () => {
     return (
       <DaySlot style={{width: 100}} min={moment('9:00am', 'h:mma').toDate()}
                   max={moment('5:00pm', 'h:mma').toDate()}
                   slices={5}
                   now={moment('9:20:01am', 'h:mm:ssa').toDate()}
+               formatter={({start, end}) => {
+                return `${moment(start).format('h:mm')}-${
+                moment(end).format('h:mm')}`
+               }}
       />
     )
   })

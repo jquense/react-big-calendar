96835fdaa769b0547c03abe08afb9c55c41602d8
Gregory Beaver <greg@chiaraquartet.net>
2016-05-25 09:28:57 -0500

    move to using localizer instead of moment

--- src/components/TimeSlice.jsx
+++ src/components/TimeSlice.jsx
@@ -1,6 +1,8 @@
 import React, { PropTypes, Component } from 'react'
 import cn from 'classnames'
 
+import localizer from '../localizer'
+
 export default class TimeSlice extends Component {
   static propTypes = {
     time: PropTypes.func.isRequired,
@@ -8,20 +10,25 @@ export default class TimeSlice extends Component {
     isNow: PropTypes.bool,
     classNames: PropTypes.string,
     showlabel: PropTypes.bool,
-    selected: PropTypes.bool
+    selected: PropTypes.bool,
+    format: PropTypes.string,
+    culture: PropTypes.string
   }
 
   static defaultProps = {
     time: () => '',
     isNow: false,
     showlabel: true,
-    selected: false
+    selected: false,
+    culture: 'en'
   }
 
   render() {
     return (
       <div className={cn('rbc-time-slot', this.props.isNow ? 'rbc-now' : '', this.props.classNames)}>
-        <span>{this.props.showlabel ? this.props.time(this.props.value) : ''}</span>
+        <span>{this.props.showlabel ?
+          localizer.format(this.props.value, this.props.format, this.props.culture)
+          : ''}</span>
       </div>
     )
   }

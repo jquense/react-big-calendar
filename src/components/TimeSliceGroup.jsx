14854151af24d6929aa7cc1ee688eb99b5b3194f
Gregory Beaver <greg@chiaraquartet.net>
2016-05-25 09:30:00 -0500

    remove size, move to localizer instead of format, minor formatting

--- src/components/TimeSliceGroup.jsx
+++ src/components/TimeSliceGroup.jsx
@@ -6,48 +6,54 @@ import date from '../utils/dates.js'
 export default class TimeSliceGroup extends Component {
   static propTypes = {
     slices: PropTypes.oneOf([2,3,4,5]),
-    size: PropTypes.number.isRequired,
-    time: PropTypes.func.isRequired,
+    step: PropTypes.number.isRequired,
     value: PropTypes.instanceOf(Date).isRequired,
     timesliceComponent: PropTypes.oneOf([TimeSlice, SelectableTimeSlice]),
     showlabels: PropTypes.bool,
     isNow: PropTypes.bool,
+    timegutterFormat: PropTypes.string,
     timesliceClassnames: PropTypes.string,
-    debug: PropTypes.bool
+    culture: PropTypes.string
   }
   static defaultProps = {
     slices: 2,
+    step: 10,
     isNow: false,
-    time: () => null,
     showlabels: true,
     timesliceComponent: TimeSlice,
-    debug: false
+    timesliceClassnames: '',
+    culture: 'en'
   }
 
   renderSlice(i, className, value) {
     const T = this.props.timesliceComponent
-    return <T key={i} showlabel={this.props.showlabels && !i} time={this.props.time}
-              isNow={this.props.isNow} classNames={className} value={value} />
+    return <T key={i}
+              className={className}
+              showlabel={this.props.showlabels && !i}
+              format={this.props.timegutterFormat}
+              culture={this.props.culture}
+              isNow={this.props.isNow}
+              value={value} />
   }
 
   renderSlices() {
-    let className = this.props.timesliceClassnames ? `${this.props.timesliceClassnames} ` : ''
+    let className = this.props.timesliceClassnames
     switch (this.props.slices) {
       case 2 :
         break
       case 3 :
-        className += 'three'
+        className += ' three'
         break
       case 4 :
-        className += 'four'
+        className += ' four'
         break
       case 5 :
       default :
-        className += 'five'
+        className += ' five'
         break
     }
     const ret = []
-    const sliceLength = Math.floor(this.props.size / this.props.slices)
+    const sliceLength = this.props.step
     let sliceValue = this.props.value
     for (let i = 0; i < this.props.slices; i++) {
       ret.push(this.renderSlice(i, className, sliceValue))

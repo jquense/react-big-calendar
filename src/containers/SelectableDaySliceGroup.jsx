d96232305cc7e767eae49c932b133bcba2fe049d
Gregory Beaver <greg@chiaraquartet.net>
2016-05-28 11:56:27 -0500

    remove unused legacy file

--- src/containers/SelectableDaySliceGroup.jsx
+++
@@ -1,6 +0,0 @@
-import Selection from './Selection.jsx'
-import DaySliceGroup from '../components/DaySliceGroup.jsx'
-
-const SelectableDaySliceGroup = Selection(DaySliceGroup)
-
-export default SelectableDaySliceGroup

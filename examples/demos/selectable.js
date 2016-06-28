82ecd3e119fd89f91daae58c10d62a8f749b1ae9
Manuel Díez <manutenfruits@gmail.com>
2016-06-28 17:02:29 +0200

    Added scrollTime prop like in FullCalendar (#122)
    * Added scrollTime prop
    
    * Some var renaming
    
    * Applying scroll on componentDidUpdate and componentDidMount

--- examples/demos/selectable.js
+++ examples/demos/selectable.js
@@ -14,6 +14,7 @@ let Selectable = React.createClass({
           selectable
           events={events}
           defaultView='week'
+          scrollToTime={new Date(1970, 1, 1, 6)}
           defaultDate={new Date(2015, 3, 1)}
           onSelectEvent={event => alert(event.title)}
           onSelectSlot={(slotInfo) => alert(

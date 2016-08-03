282b1f0b0fb8efe38abed1bdf06cbbdb2a2d27b4
Sean Bullock <sean@bluespore.com>
2016-08-03 17:57:08 +1000

    Add prop to disable the truncating of event lists in the month view

--- src/Month.jsx
+++ src/Month.jsx
@@ -133,13 +133,16 @@ let MonthView = React.createClass({
   },
 
   renderWeek(week, weekIdx, content) {
+  	console.log(this.props);
     let { first, last } = endOfRange(week);
     let evts = eventsForWeek(this.props.events, week[0], week[week.length - 1], this.props)
 
     evts.sort((a, b) => sortEvents(a, b, this.props))
 
     let segments = evts = evts.map(evt => eventSegments(evt, first, last, this.props))
-    let limit = (this.state.rowLimit - 1) || 1;
+    let limit = this.props.truncateEvents ? (this.state.rowLimit - 1) || 1 : Infinity;
+    console.log('this.props.truncateEvents', this.props.truncateEvents);
+    console.log('limit is', limit);
 
     let { levels, extra } = eventLevels(segments, limit)
 

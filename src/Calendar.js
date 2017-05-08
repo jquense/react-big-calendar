08fce146845410b0dc6ebe68f690602871a947ea
Jason Quense <monastic.panic@gmail.com>
2017-05-08 10:03:05 -0400

    WIP

--- src/Calendar.js
+++ src/Calendar.js
@@ -9,6 +9,7 @@ import {
   , dateRangeFormat
   , views as componentViews } from './utils/propTypes';
 
+import { createAccessors } from './utils/accessors'
 import { notify } from './utils/helpers';
 import { navigate, views } from './utils/constants';
 import defaultFormats from './formats';

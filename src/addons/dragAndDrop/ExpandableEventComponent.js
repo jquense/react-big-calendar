dbeb3015870a0f72828dcd9fb564520aabe8b3d1
John Mandalakas <john@mandalakas.com>
2017-05-20 13:34:24 -0500

    Make resize anchor draggable

--- src/addons/dragAndDrop/ExpandableEventComponent.js
+++ src/addons/dragAndDrop/ExpandableEventComponent.js
@@ -1,17 +1,33 @@
 import PropTypes from 'prop-types';
 import React from 'react';
+import { DragSource } from 'react-dnd';
 
 class ExpandableEventComponent extends React.Component {
   render () {
+    const { title, connectDragSource } = this.props;
     return (
       <div className="rbc-addons-dnd-expandable-event">
-        {this.props.title}
-        <div className="rbc-addons-dnd-expand-anchor">
-          <div className="rbc-addons-dnd-expand-icon" />
-        </div>
+        {title}
+        {
+          connectDragSource(
+            <div className="rbc-addons-dnd-expand-anchor">
+              <div className="rbc-addons-dnd-expand-icon" />
+            </div>
+          )
+        }
       </div>
     );
   }
 }
 
-export default ExpandableEventComponent;
+const eventSource = {
+  beginDrag: (props) => props.event
+}
+
+const collectSource = (connect, monitor) =>
+  ({
+    connectDragSource: connect.dragSource(),
+    isDragging: monitor.isDragging()
+  })
+
+export default DragSource('resize', eventSource, collectSource)(ExpandableEventComponent);

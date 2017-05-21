c84f2a445565451202a067e2e402d070491418d5
John Mandalakas <john@mandalakas.com>
2017-05-21 15:12:26 -0500

    Add example code for using the resizable event feature

--- examples/demos/dnd.js
+++ examples/demos/dnd.js
@@ -35,12 +35,40 @@ class Dnd extends React.Component {
     alert(`${event.title} was dropped onto ${event.start}`);
   }
 
+  resizeEvent = (resizeType, { event, end }) => {
+    const { events } = this.state;
+
+    // if we want to update the event while dragging we need to find the event
+    // by an identifier. here we use the title and the start time, but depending
+    // on use case you may want have a truly unique id attribute in the event
+    const nextEvents = events
+      .map(existingEvent =>
+        (existingEvent.start == event.start &&
+         existingEvent.title == event.title) ?
+         { ...existingEvent, end } : existingEvent);
+
+   // if we only care about updating the event onDrop, we could rely on finding
+   // the event through Array.prototype.indexOf like in the moveEvent callback ie:
+   // if (resizeType !== 'drop') return;
+   // const idx = events.indexOf(event);
+   // const updatedEvent = { ...event, end };
+
+   // const nextEvents = [...events]
+   // nextEvents.splice(idx, 1, updatedEvent)
+
+    this.setState({
+      events: nextEvents
+    });
+  }
+
   render(){
     return (
       <DragAndDropCalendar
         selectable
         events={this.state.events}
         onEventDrop={this.moveEvent}
+        resizable
+        onEventResize={this.resizeEvent}
         defaultView='week'
         defaultDate={new Date(2015, 3, 12)}
       />

c7b342906e1b8d496614a9e3aa65218db0de0e51
Sidney Tang <sidney.tang@elationhealth.com>
2017-05-31 10:00:26 +1200

    Undoing timezone hack since root cause of the issue it was trying to solve was elsewhere

--- src/TimeGrid.js
+++ src/TimeGrid.js
@@ -283,11 +283,7 @@ export default class TimeGrid extends Component {
     return range.map((date, i) => {
       let drilldownView = getDrilldownView(date);
 
-      // HACK: note date here is on browser timezone, we're dropping the time and displaying
-      // the date in given localizer timezone, if it's not matching the browser timezone
-      let dateStr = date.getFullYear() + '/' + (date.getMonth()+1) + '/' + date.getDate();
-      let displayDate = localizer.parse(dateStr, 'YYYY/MM/DD');
-      let label = localizer.format(displayDate, dayFormat, culture);
+      let label = localizer.format(date, dayFormat, culture);
 
       let header = (
         <HeaderComponent

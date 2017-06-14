4a78350fd78bdfefebee1d96fb3fb1e0b99309eb
Haobo Chen <haobo.chen@appfolio.com>
2017-06-13 17:39:57 -0700

    hc - Add allDayEventsLimit to timed views

---
+++ test/components/TimeGrid.spec.js
@@ -0,0 +1,73 @@
+/* eslint-env mocha */
+import React from 'react';
+import sinon from 'sinon';
+import { shallow } from 'enzyme';
+import { expect } from 'chai';
+import moment from 'moment';
+
+import * as Helpers from '../../src/utils/helpers';
+
+import TimeGrid from '../../src/TimeGrid';
+import DateContentRow from '../../src/DateContentRow';
+
+describe('<TimeGrid />', () => {
+  let props;
+  let sandbox;
+  let titleAccessor;
+  let allDayAccessor;
+  let startAccessor;
+  let endAccessor;
+  let getDrilldownView;
+
+  beforeEach(() => {
+    sandbox = sinon.sandbox.create();
+    titleAccessor = sandbox.stub();
+    allDayAccessor = sandbox.stub();
+    startAccessor = sandbox.stub();
+    endAccessor = sandbox.stub();
+    getDrilldownView = sandbox.stub().returns('agenda');
+
+    props = {
+      events: [],
+      components: {},
+      range:[],
+      titleAccessor,
+      allDayAccessor,
+      startAccessor,
+      endAccessor,
+      getDrilldownView,
+      allDayEventsLimit: 10,
+    };
+  });
+
+  afterEach(() => sandbox.restore());
+
+  describe('on componentDidMount', () => {
+    it('passes allDayEventsLimit to DateContentRow maxRows prop', () => {
+      const wrapper = shallow(<TimeGrid {...props} />);
+      const contentRow = wrapper.find(DateContentRow);
+
+      expect(contentRow).to.have.prop('maxRows', 10);
+    });
+  });
+
+  describe('handleShowMore', () => {
+    it('calls clearSelection', () => {
+      const wrapper = shallow(<TimeGrid {...props} />);
+      sandbox.stub(wrapper.instance(), 'clearSelection');
+      wrapper.instance().handleShowMore();
+
+      sinon.assert.calledOnce(wrapper.instance().clearSelection);
+    });
+
+    it('calls notify with DrilldownView', () => {
+      props.onDrillDown = 'agenda mock';
+      const date = 'mock';
+      const wrapper = shallow(<TimeGrid {...props} />);
+      sandbox.stub(Helpers, 'notify');
+      wrapper.instance().handleShowMore([], date);
+
+      sinon.assert.calledWith(Helpers.notify, 'agenda mock', ['mock', 'agenda']);
+    });
+  });
+});
---
+++ test/factories/events.js
@@ -0,0 +1,8 @@
+export default [
+  {
+    'title': 'All Day Event',
+    'allDay': true,
+    'start': new Date(2015, 3, 0),
+    'end': new Date(2015, 3, 0)
+  }
+]

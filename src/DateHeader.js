0ccbc13fa4ef3a3306c6cd461fa1a37e3b527117
andresgutgon <andresgutgon@gmail.com>
2017-06-21 15:44:49 +0200

    Fixed typo on attribute name

--- src/DateHeader.js
+++ src/DateHeader.js
@@ -1,8 +1,8 @@
 import PropTypes from 'prop-types';
 import React from 'react'
 
-const DateHeader = ({ label, drillDownView, onDrillDown }) => {
-    if (!drillDownView) {
+const DateHeader = ({ label, drilldownView, onDrillDown }) => {
+    if (!drilldownView) {
       return (<span>{label}</span>)
     }
 
@@ -16,7 +16,7 @@ const DateHeader = ({ label, drillDownView, onDrillDown }) => {
 DateHeader.propTypes = {
   label: PropTypes.node,
   date: PropTypes.instanceOf(Date),
-  drillDownView: PropTypes.string,
+  drilldownView: PropTypes.string,
   onDrillDown: PropTypes.func,
   isOffRange: PropTypes.bool
 }

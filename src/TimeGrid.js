e5cb5f9accb3fc8969b484085f3745f61b369722
Chris Horuk <chris.horuk@appfolio.com>
2017-06-27 17:45:27 -0700

    ch - fix incorrect prop type on time grid

--- src/TimeGrid.js
+++ src/TimeGrid.js
@@ -59,7 +59,10 @@ export default class TimeGrid extends Component {
     onSelectEnd: PropTypes.func,
     onSelectStart: PropTypes.func,
     onSelectEvent: PropTypes.func,
-    onDrillDown: PropTypes.func,
+    onDrillDown: PropTypes.oneOfType([
+      PropTypes.func,
+      PropTypes.string,
+    ]),
     getDrilldownView: PropTypes.func.isRequired,
     allDayEventsLimit: PropTypes.number,
 

2aa1e1c3387f60de2bf0401f3f7db9906325b8cb
Benjamin Kniffler <bkniffler@me.com>
2017-08-22 16:04:02 +0200

    Add caution notice

--- src/Calendar.js
+++ src/Calendar.js
@@ -243,7 +243,8 @@ class Calendar extends React.Component {
 
    /**
     * Optionally provide a function that returns an object of className or style props
-    * to be applied to the the time-slot node.
+    * to be applied to the the time-slot node. Caution! Styles that change layout or 
+    * position may break the calendar in unexpected ways.
     *
     * ```js
     * (

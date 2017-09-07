c216d8a3db708736e69d02fa765b6cc33d4cbf31
Nathaniel Cook <Nathaniel.Cook@target.com>
2017-09-07 14:35:25 -0500

    Compare full date objects

--- src/utils/dayViewLayout.js
+++ src/utils/dayViewLayout.js
@@ -130,12 +130,17 @@ let constructEvent = (title, start, end) => {
 }
 
 let handleMultiDayEvents = (title, start, end, current) => {
-  let s = new Date(start).getDate()
-  let e = new Date(end).getDate()
-  let c = new Date(current).getDate()
+  let s = new Date(start)
+  let e = new Date(end)
+  let c = new Date(current)
+
+  // use noon to compare dates to avoid DST issues
+  s.setHours(12, 0, 0, 0)
+  e.setHours(12, 0, 0, 0)
+  c.setHours(12, 0, 0, 0)
 
   // if current day is at the start, but spans multiple days, correct the end
-  if (c === s && c < e) {
+  if (+c === +s && c < e) {
     return constructEvent(title, start, dates.endOf(start, 'day'))
   }
 
@@ -145,7 +150,7 @@ let handleMultiDayEvents = (title, start, end, current) => {
   }
 
   // if current day is at the end of a multi day event, start at midnight to the end
-  else if (c > s && c === e) {
+  else if (c > s && +c === +e) {
     return constructEvent(title, current, end)
   }
 }

94f9da757a2f23bfebe14444f4a7008fb02f5139
Julha <julien.chaput@live.fr>
2017-10-05 15:27:24 +0200

    add onEventResize() callback to the calendar

--- src/addons/dragAndDrop/backgroundWrapper.js
+++ src/addons/dragAndDrop/backgroundWrapper.js
@@ -93,9 +93,31 @@ DraggableBackgroundWrapper.propTypes = propTypes;
 
 DraggableBackgroundWrapper.contextTypes = {
   onEventDrop: PropTypes.func,
+  onEventResize: PropTypes.func,
   dragDropManager: PropTypes.object,
   startAccessor: accessor,
   endAccessor: accessor
+}
+
+function getEventResizeTimes(event, value, startAccessor) { // calculate new start/end when resizing an event
+
+  // resizing down
+  var start = get(event, startAccessor);
+  var end = value;
+
+  // resizing up, when resizer pass above event start
+  var isPassAbove = (start >= end);
+  if (isPassAbove) {
+    var temp = start;
+    start = end;
+    end = temp;
+  }
+
+  return {
+    event: event,
+    start,
+    end
+  };
 }
 
 function createWrapper(type) {
@@ -111,19 +133,49 @@ function createWrapper(type) {
   const dropTarget = {
     drop(_, monitor, { props, context }) {
       const event = monitor.getItem();
+      const itemType = monitor.getItemType();
       const { value } = props
       const { onEventDrop, startAccessor, endAccessor } = context
-      const start = get(event, startAccessor);
-      const end = get(event, endAccessor);
 
-      onEventDrop({
-        event,
-        ...getEventTimes(start, end, value, type)
-      })
+
+      var propsCallback;
+      if (itemType === 'event-resize') { // handle after resize, drop and calculate start/end like when resizing
+        propsCallback = getEventResizeTimes(event, value, startAccessor);
+
+      } else if (itemType === 'event') {
+        var start = get(event, startAccessor);
+        var end = get(event, endAccessor);
+        propsCallback = Object.assign({
+          event: event
+        }, getEventTimes(start, end, value, type));
+
+      }
+
+      onEventDrop(propsCallback);
+
+    },
+    hover: function hover(_, monitor, { props, context }) { // new hover callback
+      const event = monitor.getItem();
+      const itemType = monitor.getItemType();
+      const { value } = props
+      const { onEventResize, startAccessor } = context
+
+
+      if ((!window.reactBigCalendarResizerValue || window.reactBigCalendarResizerValue.toString() !== value.toString())) { // TODO > find a better solution
+        window.reactBigCalendarResizerValue = value; // store value to not trigger event each time
+
+        var propsCallback;
+        if (itemType === 'event-resize') {
+          propsCallback = getEventResizeTimes(event, value, startAccessor);
+        } else if (itemType === 'event') {
+          propsCallback = { event, value };
+        }
+        onEventResize(itemType, propsCallback); // callback
+      }
     }
   };
 
-  return DropTarget(['event'], dropTarget, collectTarget)(DraggableBackgroundWrapper);
+  return DropTarget(['event', 'event-resize'], dropTarget, collectTarget)(DraggableBackgroundWrapper);
 }
 
 export const DateCellWrapper = createWrapper('dateCellWrapper');

5d7af3c0159c08cc6c493ca6f6cceb8cb1c96feb
Adam Recvlohe <adam.recvlohe@gmail.com>
2017-10-06 13:15:18 -0400

    Add Resize Functions
    Summary: Add the functionality into the DnD Calendar that will handle the resizing of the events.

--- examples/demos/dnd.js
+++ examples/demos/dnd.js
@@ -1,8 +1,8 @@
-import React from 'react'
-import events from '../events'
-import HTML5Backend from 'react-dnd-html5-backend'
-import { DragDropContext } from 'react-dnd'
-import BigCalendar from 'react-big-calendar'
+import React from 'react';
+import events from '../events';
+import HTML5Backend from 'react-dnd-html5-backend';
+import { DragDropContext } from 'react-dnd';
+import BigCalendar from 'react-big-calendar';
 import withDragAndDrop from 'react-big-calendar/lib/addons/dragAndDrop';
 
 import 'react-big-calendar/lib/addons/dragAndDrop/styles.less';
@@ -10,13 +10,13 @@ import 'react-big-calendar/lib/addons/dragAndDrop/styles.less';
 const DragAndDropCalendar = withDragAndDrop(BigCalendar);
 
 class Dnd extends React.Component {
-  constructor (props) {
-    super(props)
+  constructor(props) {
+    super(props);
     this.state = {
-      events: events
-    }
+      events: events,
+    };
 
-    this.moveEvent = this.moveEvent.bind(this)
+    this.moveEvent = this.moveEvent.bind(this);
   }
 
   moveEvent({ event, start, end }) {
@@ -25,27 +25,56 @@ class Dnd extends React.Component {
     const idx = events.indexOf(event);
     const updatedEvent = { ...event, start, end };
 
-    const nextEvents = [...events]
-    nextEvents.splice(idx, 1, updatedEvent)
+    const nextEvents = [...events];
+    nextEvents.splice(idx, 1, updatedEvent);
 
     this.setState({
-      events: nextEvents
-    })
+      events: nextEvents,
+    });
 
     alert(`${event.title} was dropped onto ${event.start}`);
   }
 
-  render(){
+  handleEventResize = (resizeType, { event, start, end }) => {
+    const { events } = this.state;
+
+    // if we want to update the event while dragging we need to find the event
+    // by an identifier. here we use the title and the start time, but depending
+    // on use case you may want have a truly unique id attribute in the event
+
+    const nextEvents = events.map(existingEvent => {
+      return existingEvent.start == event.start && existingEvent.title == event.title
+        ? { ...existingEvent, start, end }
+        : existingEvent;
+    });
+
+    // if we only care about updating the event onDrop, we could rely on finding
+    // the event through Array.prototype.indexOf like in the moveEvent callback ie:
+    // if (resizeType !== 'drop') return;
+    // const idx = events.indexOf(event);
+    // const updatedEvent = { ...event, end };
+
+    // const nextEvents = [...events]
+    // nextEvents.splice(idx, 1, updatedEvent)
+
+    this.setState({
+      events: nextEvents,
+    });
+  };
+
+  render() {
     return (
       <DragAndDropCalendar
         selectable
         events={this.state.events}
         onEventDrop={this.moveEvent}
-        defaultView='week'
+        defaultView="month"
+        resizable
+        onEventResize={this.handleEventResize}
         defaultDate={new Date(2015, 3, 12)}
       />
-    )
+    );
   }
 }
 
-export default DragDropContext(HTML5Backend)(Dnd)
+export default DragDropContext(HTML5Backend)(Dnd);

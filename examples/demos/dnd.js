27e6522d7af6b7b37e11b10b631fd350d73d3a17
Adam Recvlohe <adam.recvlohe@gmail.com>
2017-11-07 11:34:32 -0500

    Update: DnD example

--- examples/demos/dnd.js
+++ examples/demos/dnd.js
@@ -2,8 +2,8 @@ import React from 'react';
 import events from '../events';
 import HTML5Backend from 'react-dnd-html5-backend';
 import { DragDropContext } from 'react-dnd';
-import BigCalendar from 'react-big-calendar';
-import withDragAndDrop from 'react-big-calendar/lib/addons/dragAndDrop';
+import BigCalendar from '../../src';
+import withDragAndDrop from '../../src/addons/dragAndDrop';
 import { ContextMenu, MenuItem } from 'react-contextmenu';
 
 import ResizableMonthEvent from '../../src/addons/dragAndDrop/ResizableMonthEvent';
@@ -45,24 +45,10 @@ class Dnd extends React.Component {
   handleEventResize = (resizeType, { event, start, end }) => {
     const { events } = this.state;
 
-    // if we want to update the event while dragging we need to find the event
-    // by an identifier. here we use the title and the start time, but depending
-    // on use case you may want have a truly unique id attribute in the event
-
-    const nextEvents = events.map(existingEvent => {
-      return existingEvent.start == event.start && existingEvent.title == event.title
-        ? { ...existingEvent, start, end }
-        : existingEvent;
+    const nextEvents = events.map(prevEvent => {
+      if (event.id !== prevEvent.id) return prevEvent;
+      return { ...prevEvent, start, end };
     });
-
-    // if we only care about updating the event onDrop, we could rely on finding
-    // the event through Array.prototype.indexOf like in the moveEvent callback ie:
-    // if (resizeType !== 'drop') return;
-    // const idx = events.indexOf(event);
-    // const updatedEvent = { ...event, end };
-
-    // const nextEvents = [...events]
-    // nextEvents.splice(idx, 1, updatedEvent)
 
     this.setState({
       events: nextEvents,

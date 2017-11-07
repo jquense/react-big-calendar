6efcc946eb063c87391577e3c136f65815d1ab64
Adam Recvlohe <adam.recvlohe@gmail.com>
2017-11-07 11:26:30 -0500

    Apply: Add cuid to events

--- examples/events.js
+++ examples/events.js
@@ -1,5 +1,8 @@
+import cuid from 'cuid';
+
 export default [
   {
+    id: cuid(),
     title: 'All Day Event',
     allDay: true,
     start: new Date(2015, 3, 0),
@@ -7,6 +10,7 @@ export default [
     weight: 100,
   },
   {
+    id: cuid(),
     title: 'Long Event',
     start: new Date(2015, 3, 7),
     end: new Date(2015, 3, 10),
@@ -24,6 +28,7 @@ export default [
     weight: 200,
   },
   {
+    id: cuid(),
     title: 'All Day Event',
     allDay: true,
     start: new Date(2015, 3, 7),
@@ -31,30 +36,35 @@ export default [
     weight: 200,
   },
   {
+    id: cuid(),
     title: 'DTS STARTS',
     start: new Date(2016, 2, 13, 0, 0, 0),
     end: new Date(2016, 2, 20, 0, 0, 0),
     weight: 300,
   },
   {
+    id: cuid(),
     title: 'DTS ENDS',
     start: new Date(2016, 10, 6, 0, 0, 0),
     end: new Date(2016, 10, 13, 0, 0, 0),
     weight: 400,
   },
   {
+    id: cuid(),
     title: 'Some Event',
     start: new Date(2015, 3, 9, 0, 0, 0),
     end: new Date(2015, 3, 9, 0, 0, 0),
     weight: 100,
   },
   {
+    id: cuid(),
     title: 'Some Event 2',
     start: new Date(2015, 3, 9, 0, 0, 0),
     end: new Date(2015, 3, 9, 0, 0, 0),
     weight: 200,
   },
   {
+    id: cuid(),
     title: 'Conference',
     start: new Date(2015, 3, 11),
     end: new Date(2015, 3, 13),
@@ -62,6 +72,7 @@ export default [
     weight: 100,
   },
   {
+    id: cuid(),
     title: 'Meeting',
     start: new Date(2015, 3, 12, 10, 30, 0, 0),
     end: new Date(2015, 3, 12, 12, 30, 0, 0),
@@ -69,6 +80,7 @@ export default [
     weight: 200,
   },
   {
+    id: cuid(),
     title: 'Lunch',
     start: new Date(2015, 3, 12, 12, 0, 0, 0),
     end: new Date(2015, 3, 12, 13, 0, 0, 0),
@@ -76,12 +88,14 @@ export default [
     weight: 300,
   },
   {
+    id: cuid(),
     title: 'Meeting',
     start: new Date(2015, 3, 12, 14, 0, 0, 0),
     end: new Date(2015, 3, 12, 15, 0, 0, 0),
     weight: 400,
   },
   {
+    id: cuid(),
     title: 'Happy Hour',
     start: new Date(2015, 3, 12, 17, 0, 0, 0),
     end: new Date(2015, 3, 12, 17, 30, 0, 0),
@@ -89,24 +103,28 @@ export default [
     weight: 500,
   },
   {
+    id: cuid(),
     title: 'Dinner',
     start: new Date(2015, 3, 12, 20, 0, 0, 0),
     end: new Date(2015, 3, 12, 21, 0, 0, 0),
     weight: 600,
   },
   {
+    id: cuid(),
     title: 'Birthday Party',
     start: new Date(2015, 3, 13, 7, 0, 0),
     end: new Date(2015, 3, 13, 10, 30, 0),
     weight: 1200,
   },
   {
+    id: cuid(),
     title: 'Late Night Event',
     start: new Date(2015, 3, 17, 19, 30, 0),
     end: new Date(2015, 3, 18, 2, 0, 0),
     weight: 1300,
   },
   {
+    id: cuid(),
     title: 'Multi-day Event',
     start: new Date(2015, 3, 20, 19, 30, 0),
     end: new Date(2015, 3, 22, 2, 0, 0),

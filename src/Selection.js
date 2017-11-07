a3abd9fad7fdd2cd6a98bb31c2c640e81719bddc
AlexMarvelo <mironenkoalexey@gmail.com>
2017-11-07 17:35:22 +0200

    #563: Clear _lastClickData on dblClick event

--- src/Selection.js
+++ src/Selection.js
@@ -257,16 +257,9 @@ class Selection {
     const { pageX, pageY, clientX, clientY } = getEventCoordinates(e);
     const now = new Date().getTime();
 
-    if (
-        this._lastClickData &&
-        !this._lastClickData.isDblClick &&
-        now - this._lastClickData.timestamp < clickInterval
-    ) {
+    if (this._lastClickData && now - this._lastClickData.timestamp < clickInterval) {
       // Double click event
-      this._lastClickData = {
-        timestamp: now,
-        isDblClick: true,
-      };
+      this._lastClickData = null;
       return this.emit('doubleClick', {
         x: pageX,
         y: pageY,
@@ -275,6 +268,7 @@ class Selection {
       })
     }
 
+    // Click event
     this._lastClickData = {
       timestamp: now,
     };

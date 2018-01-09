268fd6f02bb47fa8ff2cbe17ad8f68df1020f30d
Tobias Andersen <j.tobiasandersen@gmail.com>
2018-01-09 23:06:38 +0100

    fix inRange check

--- src/utils/eventLevels.js
+++ src/utils/eventLevels.js
@@ -71,7 +71,7 @@ export function inRange(e, start, end, { startAccessor, endAccessor }) {
 
   let startsBeforeEnd = dates.lte(eStart, end, 'day')
   // when the event is zero duration we need to handle a bit differently
-  let endsAfterStart = !dates.eq(eStart, eEnd)
+  let endsAfterStart = dates.eq(eStart, eEnd)
     ? dates.gt(eEnd, start, 'day')
     : dates.gte(eEnd, start, 'day')
 

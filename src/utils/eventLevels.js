7788f1070826d318fe55cdabcb66470d5d924866
Jason Quense <monastic.panic@gmail.com>
2018-01-10 09:52:17 -0500

    fix style and day layout

--- src/utils/eventLevels.js
+++ src/utils/eventLevels.js
@@ -71,9 +71,9 @@ export function inRange(e, start, end, { startAccessor, endAccessor }) {
 
   let startsBeforeEnd = dates.lte(eStart, end, 'day')
   // when the event is zero duration we need to handle a bit differently
-  let endsAfterStart = !dates.eq(eStart, eEnd)
-    ? dates.gt(eEnd, start, 'day')
-    : dates.gte(eEnd, start, 'day')
+  let endsAfterStart = !dates.eq(eStart, eEnd, 'minutes')
+    ? dates.gt(eEnd, start, 'minutes')
+    : dates.gte(eEnd, start, 'minutes')
 
   return startsBeforeEnd && endsAfterStart
 }

543301672e0a51af2c3dd1a567ae26a6a78daba2
Akia Vongdara <vongdarakia@gmail.com>
2018-03-05 14:08:41 -0800

    Add title to headers

--- src/TimeGrid.js
+++ src/TimeGrid.js
@@ -338,6 +338,7 @@ export default class TimeGrid extends Component {
     const today = getNow()
     return range.map((date, i) => {
       return resources.map((resource, j) => {
+        const resourceName = get(resource, resourceTitleAccessor)
         return (
           <div
             key={i + '-' + j}
@@ -347,7 +348,7 @@ export default class TimeGrid extends Component {
             )}
             style={segStyle(1, this.slots)}
           >
-            <span>{get(resource, resourceTitleAccessor)}</span>
+            <span title={resourceName}>{resourceName}</span>
           </div>
         )
       })

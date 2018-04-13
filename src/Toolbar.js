5749763b680b81d1d1d541b4bfd3fd18c4de342c
Rich Manalang <rmanalang@atlassian.com>
2018-04-13 14:23:55 -0700

    v0.19.4 Arrows for toolbar nav

--- src/Toolbar.js
+++ src/Toolbar.js
@@ -23,7 +23,13 @@ class Toolbar extends React.Component {
             type="button"
             onClick={this.navigate.bind(null, navigate.PREVIOUS)}
           >
-            {messages.previous}
+            <svg width="24" height="24" viewBox="0 0 24 24">
+              <path
+                d="M8.414 11.5H18a1 1 0 0 1 0 2H8.414l3.793 3.793a1 1 0 0 1-1.414 1.414l-5.5-5.5a1 1 0 0 1 0-1.414l5.5-5.5a1 1 0 0 1 1.414 1.414L8.414 11.5z"
+                fill="currentColor"
+                fillRule="evenodd"
+              />
+            </svg>
           </button>
           <button
             type="button"
@@ -35,7 +41,13 @@ class Toolbar extends React.Component {
             type="button"
             onClick={this.navigate.bind(null, navigate.NEXT)}
           >
-            {messages.next}
+            <svg width="24" height="24" viewBox="0 0 24 24">
+              <path
+                d="M11.793 5.793a.999.999 0 0 0 0 1.414L15.586 11H6a1 1 0 0 0 0 2h9.586l-3.793 3.793a.999.999 0 0 0 0 1.414c.39.39 1.024.39 1.415 0l5.499-5.5a.997.997 0 0 0 .293-.679v-.057a.996.996 0 0 0-.293-.678l-5.499-5.5a1 1 0 0 0-1.415 0z"
+                fill="currentColor"
+                fillRule="evenodd"
+              />
+            </svg>
           </button>
         </span>
 

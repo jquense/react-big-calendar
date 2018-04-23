69197f0d28968df6554f69084afa16188b1f6733
Rich Manalang <rmanalang@atlassian.com>
2018-04-23 14:12:10 -0700

    v0.19.10 onHover events on EventWrapper

--- src/EventWrapper.js
+++ src/EventWrapper.js
@@ -1,8 +1,35 @@
+import PropTypes from 'prop-types'
 import React from 'react'
 
 class EventWrapper extends React.Component {
+  static propTypes = {
+    event: PropTypes.object.isRequired,
+    onMouseEnter: PropTypes.func,
+    onMouseLeave: PropTypes.func,
+  }
+
+  constructor(props) {
+    super(props)
+  }
+
   render() {
-    return this.props.children
+    return (
+      <div
+        ref={this.setEventRef}
+        onMouseEnter={
+          this.props.event.onMouseEnter
+            ? this.props.event.onMouseEnter.bind(null, this.props.event)
+            : null
+        }
+        onMouseLeave={
+          this.props.event.onMouseLeave
+            ? this.props.event.onMouseLeave.bind(null, this.props.event)
+            : null
+        }
+      >
+        {this.props.children}
+      </div>
+    )
   }
 }
 

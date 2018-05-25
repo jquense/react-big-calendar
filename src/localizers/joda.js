38ce460aeab22356af580b2e48c92c0a185a51f5
adam_jung <ajung91@hotmail.com>
2018-05-25 01:29:22 -0700

    begin purge of dates

--- src/localizers/joda.js
+++ src/localizers/joda.js
@@ -1,54 +1,32 @@
-import dates from '../utils/dates'
 import { set } from '../formats'
 import { set as setLocalizer } from '../localizer'
+import { DateTimeFormatter, ZonedDateTime, ZoneId, nativeJs } from 'js-joda'
 
-let dateRangeFormat = ({ start, end }, culture, local) =>
-  local.format(start, 'L', culture) + ' — ' + local.format(end, 'L', culture)
-
-let timeRangeFormat = ({ start, end }, culture, local) =>
-  local.format(start, 'LT', culture) + ' — ' + local.format(end, 'LT', culture)
-
-let timeRangeStartFormat = ({ start, end }, culture, local) =>
-  local.format(start, 'h:mma', culture) + ' — '
-
-let timeRangeEndFormat = ({ start, end }, culture, local) =>
-  ' — ' + local.format(end, 'h:mma', culture)
-
-let weekRangeFormat = ({ start, end }, culture, local) =>
-  local.format(start, 'MMM DD', culture) +
-  ' - ' +
-  local.format(end, dates.eq(start, end, 'month') ? 'DD' : 'MMM DD', culture)
-
-export let formats = {
-  dateFormat: 'DD',
-  dayFormat: 'ddd DD/MM',
-  weekdayFormat: 'ddd',
-
-  selectRangeFormat: timeRangeFormat,
-  eventTimeRangeFormat: timeRangeFormat,
-  eventTimeRangeStartFormat: timeRangeStartFormat,
-  eventTimeRangeEndFormat: timeRangeEndFormat,
-
-  timeGutterFormat: 'LT',
-
-  monthHeaderFormat: 'MMMM YYYY',
-  dayHeaderFormat: 'dddd MMM DD',
-  dayRangeHeaderFormat: weekRangeFormat,
-  agendaHeaderFormat: dateRangeFormat,
-
-  agendaDateFormat: 'ddd MMM DD',
-  agendaTimeFormat: 'LT',
-  agendaTimeRangeFormat: timeRangeFormat,
+const jsJodaZonedDtToJsDate = zdt => {
+  // Format into a correct UTC string with trailing Z and parse with new Date()
+  const jodaFormatter = DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ss'Z'")
+  const utcStr = jodaFormatter.format(zdt.withZoneSameInstant(ZoneId.of('UTC')))
+  return new Date(utcStr) // this Date is safe
 }
 
 export default function() {
-  set(formats)
+  set({})
 
   // NB: we only care about the english (EN) locale
-
   return setLocalizer({
-    firstOfWeek() {},
-    parse() {},
-    format() {},
+    firstOfWeek() {
+      return 0 // in english locale, start of week is always 0
+    },
+    parse(value) {
+      // CavEmpt: only works for javascript date and moment
+      return ZonedDateTime.from(nativeJs(value))
+    },
+    format(value, formatOptions, culture) {
+      const formatter = new Intl.DateTimeFormat(
+        [culture || 'en'],
+        formatOptions
+      )
+      return formatter.format(jsJodaZonedDtToJsDate(value))
+    },
   })
 }

672d02ed3c1ebc8501414c4f3c2805eb4ba1cb94
open-digitalp <info@digitalp.ch>
2018-06-08 07:49:02 +0200

    Fix resourceId not receive in onEventDrop

--- src/TimeGrid.js
+++ src/TimeGrid.js
@@ -184,7 +184,7 @@ export default class TimeGrid extends Component {
             {...this.props}
             min={dates.merge(date, min)}
             max={dates.merge(date, max)}
-            resource={resource && resource.id}
+            resource={resource.resourceId}
             eventComponent={components.event}
             eventWrapperComponent={components.eventWrapper}
             timeSlotWrapperComponent={components.dayWrapper}

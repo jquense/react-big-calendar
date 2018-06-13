556d73ea17140304d3060dde624429591a0c5669
sandorvasas <sonkappz@gmail.com>
2018-06-14 00:23:44 +1200

    per event selectability
    added $selectable to event fields which controls whether the event is selectable with a single click

--- examples/events.js
+++ examples/events.js
@@ -78,6 +78,7 @@ export default [
     title: 'Birthday Party',
     start: new Date(2015, 3, 13, 7, 0, 0),
     end: new Date(2015, 3, 13, 10, 30, 0),
+    $selectable: false,
   },
   {
     id: 12,

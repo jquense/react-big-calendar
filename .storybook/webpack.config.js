5a836954898b3a355555fa21adf17b3453833469
Kiichi Tachibana <bodega1123@gmail.com>
2018-11-07 22:05:11 +0900

    chore: remove extractText plugin to fix storybook (#1048)

--- .storybook/webpack.config.js
+++ .storybook/webpack.config.js
@@ -14,6 +14,6 @@ module.exports = function(config) {
         rules.less({ browsers }),
       ],
     },
-    plugins: [...config.plugins, plugins.extractText()],
+    plugins: [...config.plugins],
   }
 }

d92494082e2419349bab9c379939d8c13acbd302
Stephen Blades <stephen.blades@ul.com>
2019-05-07 10:54:11 -0500

    Update Storybook to Storybook, as well as updated config and addon registration.

--- .storybook/webpack.config.js
+++ .storybook/webpack.config.js
@@ -1,19 +1,13 @@
-const { rules, loaders, plugins, stats } = require('webpack-atoms')
+const { rules } = require('webpack-atoms')
 
 const browsers = ['last 2 versions', 'ie >= 10']
 
-module.exports = function(config) {
-  return {
-    ...config,
-    module: {
-      rules: [
-        rules.js(),
-        rules.fonts(),
-        rules.images(),
-        rules.css(),
-        rules.less({ browsers }),
-      ],
-    },
-    plugins: [...config.plugins],
-  }
+module.exports = function({ config }) {
+  config.module.rules.push(rules.js())
+  config.module.rules.push(rules.fonts())
+  config.module.rules.push(rules.images())
+  config.module.rules.push(rules.css())
+  config.module.rules.push(rules.less({ browsers }))
+
+  return config
 }

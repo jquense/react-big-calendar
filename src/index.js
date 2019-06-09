5d93c9d21202a64e149cfade7ed6a44c244d12cf
Bogdan Chadkin <trysound@yandex.ru>
2019-06-09 15:02:14 +0300

    Feat: expose date localizer (#1347)
    I use date-fns and have custom localizer. Though I need to import
    internals to create it. Would be good to have it in public api.
    
    Also did you consider to use named exports instead of Object.assign?

--- src/index.js
+++ src/index.js
@@ -1,12 +1,14 @@
 import Calendar from './Calendar'
 import EventWrapper from './EventWrapper'
 import BackgroundWrapper from './BackgroundWrapper'
+import { DateLocalizer } from './localizer'
 import momentLocalizer from './localizers/moment'
 import globalizeLocalizer from './localizers/globalize'
 import move from './utils/move'
 import { views, navigate } from './utils/constants'
 
 Object.assign(Calendar, {
+  DateLocalizer,
   globalizeLocalizer,
   momentLocalizer,
   Views: views,

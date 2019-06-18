689f74ebcd2e6ea0116c1a5353fd5109889df04a
Bogdan Chadkin <trysound@yandex.ru>
2019-06-18 16:13:52 +0300

    chore(deps): upgrade uncontrollable (#1357)
    * Update size snapshot
    
    * Upgrade uncontrollable
    
    * Upgrade to the latest version
    
    * Try to fix the build

--- src/Calendar.js
+++ src/Calendar.js
@@ -1,6 +1,6 @@
 import PropTypes from 'prop-types'
 import React from 'react'
-import uncontrollable from 'uncontrollable'
+import { uncontrollable } from 'uncontrollable'
 import cn from 'classnames'
 import {
   accessor,

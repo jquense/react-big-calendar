4c904c2f06ad7fe6f6602d04cf14bcdaeab03ad2
Adam Ginzberg <aginzberg@gmail.com>
2020-02-28 07:54:48 -0800

    fix: bump memoize-one and migrate new isEqual API (#1583)

--- src/utils/DateSlotMetrics.js
+++ src/utils/DateSlotMetrics.js
@@ -4,7 +4,8 @@ import { eventSegments, endOfRange, eventLevels } from './eventLevels'
 
 let isSegmentInSlot = (seg, slot) => seg.left <= slot && seg.right >= slot
 
-const isEqual = (a, b) => a.range === b.range && a.events === b.events
+const isEqual = (a, b) =>
+  a[0].range === b[0].range && a[0].events === b[0].events
 
 export function getSlotMetrics() {
   return memoize(options => {

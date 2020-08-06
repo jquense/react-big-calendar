2480c7f253a44494219ca589a6f5fab5ec79c26e
marco <marco.schweitzer@gmail.com>
2020-08-06 14:06:01 +0200

    modified for assigning multiple resources to an event

--- src/utils/Resources.js
+++ src/utils/Resources.js
@@ -10,9 +10,11 @@ export default function Resources(resources, accessors) {
     },
 
     groupEvents(events) {
+      // console.log("groupEvents",events);
       const eventsByResource = new Map()
 
       if (!resources) {
+        // console.log("no resources");
         // Return all events if resources are not provided
         eventsByResource.set(NONE, events)
         return eventsByResource
@@ -20,10 +22,21 @@ export default function Resources(resources, accessors) {
 
       events.forEach(event => {
         const id = accessors.resource(event) || NONE
-        let resourceEvents = eventsByResource.get(id) || []
-        resourceEvents.push(event)
-        eventsByResource.set(id, resourceEvents)
+
+        //split up if it is an array
+        if (Array.isArray(id)) {
+          id.forEach(item => {
+            let resourceEvents = eventsByResource.get(item) || []
+            resourceEvents.push(event)
+            eventsByResource.set(item, resourceEvents)
+          })
+        } else {
+          let resourceEvents = eventsByResource.get(id) || []
+          resourceEvents.push(event)
+          eventsByResource.set(id, resourceEvents)
+        }
       })
+      // console.log("eventsByResource", eventsByResource);
       return eventsByResource
     },
   }

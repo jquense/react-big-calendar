aab0b927ed48fed5cfcc509f9f906dcfab8dc0d1
Blades, Stephen <Stephen.Blades@ul.com>
2020-09-25 12:24:39 -0500

    refactor: TimeGutter to functional component

--- src/TimeGutter.js
+++ src/TimeGutter.js
@@ -1,11 +1,92 @@
+import React, { useState, useEffect } from 'react'
 import clsx from 'clsx'
 import PropTypes from 'prop-types'
-import React, { Component } from 'react'
 
 import * as TimeSlotUtils from './utils/TimeSlots'
 import TimeSlotGroup from './TimeSlotGroup'
 
-export default class TimeGutter extends Component {
+const TimeGutter = ({
+  min,
+  max,
+  timeslots,
+  step,
+  localizer,
+  getNow,
+  resource,
+  components,
+  getters,
+}) => {
+  const [slotMetrics, setSlotMetrics] = useState(
+    TimeSlotUtils.getSlotMetrics({
+      min,
+      max,
+      timeslots,
+      step,
+    })
+  )
+
+  useEffect(() => {
+    if (slotMetrics) {
+      setSlotMetrics(
+        slotMetrics.update({
+          min,
+          max,
+          timeslots,
+          step,
+        })
+      )
+    }
+    /**
+     * We don't want this to fire when slotMetrics is updated as it would recursively bomb
+     */
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [min, max, timeslots, step])
+
+  const renderSlot = (value, idx) => {
+    if (idx !== 0) return null
+
+    const isNow = slotMetrics.dateIsInGroup(getNow(), idx)
+    return (
+      <span className={clsx('rbc-label', isNow && 'rbc-now')}>
+        {localizer.format(value, 'timeGutterFormat')}
+      </span>
+    )
+  }
+
+  return (
+    <div className="rbc-time-gutter rbc-time-column">
+      {slotMetrics.groups.map((grp, idx) => {
+        return (
+          <TimeSlotGroup
+            key={idx}
+            group={grp}
+            resource={resource}
+            components={components}
+            renderSlot={renderSlot}
+            getters={getters}
+          />
+        )
+      })}
+    </div>
+  )
+}
+
+TimeGutter.propTypes = {
+  min: PropTypes.instanceOf(Date).isRequired,
+  max: PropTypes.instanceOf(Date).isRequired,
+  timeslots: PropTypes.number.isRequired,
+  step: PropTypes.number.isRequired,
+  getNow: PropTypes.func.isRequired,
+  components: PropTypes.object.isRequired,
+  getters: PropTypes.object,
+
+  localizer: PropTypes.object.isRequired,
+  resource: PropTypes.string,
+}
+
+export default TimeGutter
+
+/*export default class TimeGutter extends Component {
   constructor(...args) {
     super(...args)
 
@@ -55,17 +136,4 @@ export default class TimeGutter extends Component {
       </div>
     )
   }
-}
-
-TimeGutter.propTypes = {
-  min: PropTypes.instanceOf(Date).isRequired,
-  max: PropTypes.instanceOf(Date).isRequired,
-  timeslots: PropTypes.number.isRequired,
-  step: PropTypes.number.isRequired,
-  getNow: PropTypes.func.isRequired,
-  components: PropTypes.object.isRequired,
-  getters: PropTypes.object,
-
-  localizer: PropTypes.object.isRequired,
-  resource: PropTypes.string,
-}
+}*/

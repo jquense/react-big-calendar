f1812489c8b8f8035d23a3c3e79418b058e8f443
Nikita Levchuk <nikitalevchuk@gmail.com>
2021-02-18 16:37:54 +0700

    feat(DnD): use custom event wrapper component while dragging
    This change allows to use custom EventWrapper component on preview event

--- src/addons/dragAndDrop/EventContainerWrapper.js
+++ src/addons/dragAndDrop/EventContainerWrapper.js
@@ -9,7 +9,6 @@ import Selection, {
 } from '../../Selection'
 import TimeGridEvent from '../../TimeGridEvent'
 import { dragAccessors, eventTimes, pointInColumn } from './common'
-import NoopWrapper from '../../NoopWrapper'
 
 class EventContainerWrapper extends React.Component {
   static propTypes = {
@@ -236,7 +235,7 @@ class EventContainerWrapper extends React.Component {
               className="rbc-addons-dnd-drag-preview"
               style={{ top, height, width: 100 }}
               getters={getters}
-              components={{ ...components, eventWrapper: NoopWrapper }}
+              components={components}
               accessors={{ ...accessors, ...dragAccessors }}
               continuesEarlier={startsBeforeDay}
               continuesLater={startsAfterDay}

48f4e5a6965409addf570761e70762b918e3c412
John Lenehan <jlenehan@socialflow.com>
2021-04-01 14:57:00 -0400

    Adds support for using localizer math operators for timezone calculations

--- src/utils/TimeSlots.js
+++ src/utils/TimeSlots.js
@@ -8,12 +8,18 @@ const getKey = (min, max, step, slots) =>
   `${+dates.startOf(max, 'minutes')}` +
   `${step}-${slots}`
 
-export function getSlotMetrics({ min: start, max: end, step, timeslots }) {
+export function getSlotMetrics({
+  min: start,
+  max: end,
+  step,
+  timeslots,
+  localizer,
+}) {
   const key = getKey(start, end, step, timeslots)
 
   // if the start is on a DST-changing day but *after* the moment of DST
   // transition we need to add those extra minutes to our minutesFromMidnight
-  const daystart = dates.startOf(start, 'day')
+  const daystart = dates.startOf(start, 'day', localizer)
   const daystartdstoffset = getDstOffset(daystart, start)
   const totalMin =
     1 + dates.diff(start, end, 'minutes') + getDstOffset(start, end)

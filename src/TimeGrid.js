76e625494c3c9344c322604d1f4aaf17d3944dbd
Steve 'Cutter' Blades <no.junk@cutterscrossing.com>
2021-11-09 18:46:20 -0600

    fix: Correct scrollToTime functionailty (#2055)
    * fix: Correct scrollToTime functionailty
    
    Fix the calculateScroll method in the TimeGrid
    
    #2028 #1717
    
    * chore: Remove localIp bits
    git push
    
    * Revert "chore: Remove localIp bits"
    
    This reverts commit 0eaae8cfe3ef719dacbf4e4232c4b86ec4c7408b.
    
    * chore: Remove localIp bits

--- src/TimeGrid.js
+++ src/TimeGrid.js
@@ -315,7 +315,7 @@ export default class TimeGrid extends Component {
     const { min, max, scrollToTime, localizer } = props
 
     const diffMillis = scrollToTime - localizer.startOf(scrollToTime, 'day')
-    const totalMillis = localizer.diff(max, min)
+    const totalMillis = localizer.diff(min, max, 'milliseconds')
 
     this._scrollRatio = diffMillis / totalMillis
   }

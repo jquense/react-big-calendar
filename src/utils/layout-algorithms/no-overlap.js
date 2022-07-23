75e80d75f351c9f5f1c4d396d1666ffeecc055ea
Łukasz Bąk <lukbak1929@gmail.com>
2022-07-23 02:28:24 +0200

    fix: no-overlap layout algorithm

--- src/utils/layout-algorithms/no-overlap.js
+++ src/utils/layout-algorithms/no-overlap.js
@@ -50,8 +50,11 @@ export default function ({
       const y3 = se2.style.top
       const y4 = se2.style.top + se2.style.height
 
-      // be friends when overlapped
-      if ((y3 <= y1 && y1 <= y4) || (y1 <= y3 && y3 <= y2)) {
+      if (
+        (y3 >= y1 && y4 <= y2) ||
+        (y4 > y1 && y4 <= y2) ||
+        (y3 >= y1 && y3 < y2)
+      ) {
         // TODO : hashmap would be effective for performance
         se1.friends.push(se2)
         se2.friends.push(se1)

a449106517c7b307e7032bdce662c5a07a2740b9
Gerald Hoxha <hoxhag_gh@outlook.com>
2024-10-02 20:47:43 -0400

    fix: additional testing for different localizers (luxon and dayjs)

--- stories/Durations.stories.js
+++ stories/Durations.stories.js
@@ -5,11 +5,13 @@ import {
   momentLocalizer,
   globalizeLocalizer,
   luxonLocalizer,
+  dayjsLocalizer,
 } from 'react-big-calendar'
 import withDragAndDrop from '../src/addons/dragAndDrop'
 
 import moment from 'moment'
 import 'moment-timezone/builds/moment-timezone-with-data-1970-2030'
+import dayjs from 'dayjs'
 import globalize from 'globalize'
 import { DateTime } from 'luxon'
 
@@ -17,6 +19,7 @@ const localizers = {
   globalize: globalizeLocalizer(globalize),
   moment: momentLocalizer(moment),
   luxon: luxonLocalizer(DateTime, { firstDayOfWeek: 7 }),
+  dayjs: dayjsLocalizer(dayjs),
 }
 
 const DraggableCalendar = withDragAndDrop(Calendar)
@@ -205,8 +208,8 @@ EventsEndingAtMidnight.args = {
   ],
 }
 
-export const EventsEndingPastMidnight = Template.bind({})
-EventsEndingPastMidnight.argTypes = {
+export const EventsEndingPastMidnightMoment = Template.bind({})
+EventsEndingPastMidnightMoment.argTypes = {
   localizer: {
     options: ['globalize', 'moment', 'luxon'],
     control: {
@@ -215,7 +218,7 @@ EventsEndingPastMidnight.argTypes = {
   },
 }
 
-EventsEndingPastMidnight.args = {
+EventsEndingPastMidnightMoment.args = {
   showMultiDayTimes: true,
   defaultView: Views.WEEK,
   localizer: 'moment',
@@ -236,8 +239,8 @@ EventsEndingPastMidnight.args = {
   ],
 }
 
-export const EventsEndingBeforeMidnight = Template.bind({})
-EventsEndingBeforeMidnight.argTypes = {
+export const EventsEndingBeforeMidnightMoment = Template.bind({})
+EventsEndingBeforeMidnightMoment.argTypes = {
   localizer: {
     options: ['globalize', 'moment', 'luxon'],
     control: {
@@ -246,7 +249,7 @@ EventsEndingBeforeMidnight.argTypes = {
   },
 }
 
-EventsEndingBeforeMidnight.args = {
+EventsEndingBeforeMidnightMoment.args = {
   showMultiDayTimes: true,
   defaultView: Views.WEEK,
   localizer: 'moment',
@@ -262,6 +265,132 @@ EventsEndingBeforeMidnight.args = {
             new Date(new Date().setDate(new Date().getDate())).setHours(23)
           ).setMinutes(59)
         ).setSeconds(0)
+      ),
+    },
+  ],
+}
+
+export const EventsEndingAtMidnightLuxon = Template.bind({})
+EventsEndingAtMidnightLuxon.argTypes = {
+  localizer: {
+    options: ['globalize', 'moment', 'luxon'],
+    control: {
+      type: 'select',
+    },
+  },
+}
+
+EventsEndingAtMidnightLuxon.args = {
+  showMultiDayTimes: true,
+  defaultView: Views.WEEK,
+  localizer: 'luxon',
+  min: moment('12:00am', 'h:mma').toDate(),
+  max: moment('11:59pm', 'h:mma').toDate(),
+  events: [
+    {
+      title: 'week view issue',
+      start: new Date(new Date(new Date().setHours(11)).setMinutes(0)),
+      end: new Date(
+        new Date(
+          new Date(
+            new Date(new Date().setDate(new Date().getDate() + 1)).setHours(0)
+          ).setMinutes(0)
+        ).setSeconds(0)
+      ),
+    },
+  ],
+}
+
+export const EventsEndingPastMidnightLuxon = Template.bind({})
+EventsEndingPastMidnightLuxon.argTypes = {
+  localizer: {
+    options: ['globalize', 'moment', 'luxon', 'dayjs'],
+    control: {
+      type: 'select',
+    },
+  },
+}
+
+EventsEndingPastMidnightLuxon.args = {
+  showMultiDayTimes: true,
+  defaultView: Views.WEEK,
+  localizer: 'luxon',
+  min: moment('12:00am', 'h:mma').toDate(),
+  max: moment('11:59pm', 'h:mma').toDate(),
+  events: [
+    {
+      title: 'week view issue',
+      start: new Date(new Date(new Date().setHours(11)).setMinutes(0)),
+      end: new Date(
+        new Date(
+          new Date(
+            new Date(new Date().setDate(new Date().getDate() + 1)).setHours(0)
+          ).setMinutes(5)
+        ).setSeconds(0)
+      ),
+    },
+  ],
+}
+
+export const EventsEndingAtMidnightDayJs = Template.bind({})
+EventsEndingAtMidnightDayJs.argTypes = {
+  localizer: {
+    options: ['globalize', 'moment', 'luxon', 'dayjs'],
+    control: {
+      type: 'select',
+    },
+  },
+}
+
+EventsEndingAtMidnightDayJs.args = {
+  showMultiDayTimes: true,
+  defaultView: Views.WEEK,
+  localizer: 'dayjs',
+  min: moment('12:00am', 'h:mma').toDate(),
+  max: moment('11:59pm', 'h:mma').toDate(),
+  events: [
+    {
+      title: 'week view issue',
+      start: new Date(new Date(new Date().setHours(11)).setMinutes(0)),
+      end: new Date(
+        new Date(
+          new Date(
+            new Date(new Date().setDate(new Date().getDate() + 1)).setHours(0)
+          ).setMinutes(0)
+        ).setSeconds(0)
+      ),
+    },
+  ],
+}
+
+export const EventsEndingPastMidnightDayJs = Template.bind({})
+EventsEndingAtMidnightDayJs.argTypes = {
+  localizer: {
+    options: ['globalize', 'moment', 'luxon', 'dayjs'],
+    control: {
+      type: 'select',
+    },
+  },
+}
+
+EventsEndingPastMidnightDayJs.args = {
+  showMultiDayTimes: true,
+  defaultView: Views.WEEK,
+  localizer: 'dayjs',
+  min: moment('12:00am', 'h:mma').toDate(),
+  max: moment('11:59pm', 'h:mma').toDate(),
+  events: [
+    {
+      title: 'week view issue',
+      start: new Date(new Date(new Date().setHours(11)).setMinutes(0)),
+      end: new Date(
+        new Date(
+          new Date(
+            new Date(
+              new Date(new Date().setDate(new Date().getDate() + 1)).setHours(0)
+            ).setMinutes(5)
+          ).setSeconds(0)
+        ).setMilliseconds(0)
       ),
     },
   ],

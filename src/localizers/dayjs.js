7ba9e24e24943b1f20efcb1652097933ac3b1774
Steve 'Cutter' Blades <no.junk@cutterscrossing.com>
2025-06-16 13:31:10 -0400

    fix(dayjs localizer): reverts previous changs, as it was casting a date as UTC instead of local time (#2759)
    Previous change was casting a date as UTC instead of local time
    
    Closes #2758

--- src/localizers/dayjs.js
+++ src/localizers/dayjs.js
@@ -192,8 +192,7 @@ export default function (dayjsLib) {
     const tm = dayjs(time).format('HH:mm:ss')
     const dt = dayjs(date).startOf('day').format('MM/DD/YYYY')
     // We do it this way to avoid issues when timezone switching
-    const mergedDateTime = dayjs(`${dt} ${tm}`).toDate()
-    return dayjsLib(mergedDateTime).utc(true).toDate()
+    return dayjs(`${dt} ${tm}`).toDate()
   }
 
   function add(date, adder, unit) {
